e37b86b4eb38286760c8a8a87560f6333d80ddb0
gus0660 <guillaumeroch3@gmail.com>
2024-01-24 12:10:19 +0100

    sauveguardes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function craiVar(element) {
-    return document.querySelector(element)
-};
+  return document.querySelector(element);
+}
 const auDessus = craiVar("#auDessus");
 const grosTitre = craiVar("#grosTitre");
 const taskApp = craiVar("#taskApp");
@@ -8,8 +8,8 @@ const newTask = craiVar("#newTask");
 const addTask = craiVar("#addTask");
 const tasksList = craiVar("#tasksList");
 // il faut : créé une détection du click sur "ajouter" donc "addEventListener" sur "addTask"
-addTask.addEventListener("click",() => {
-    // qui génère une variable "valeur" qui correspont à la "value" de "newTask"
-    let valeur = newTask.value;
-    console.log(valeur)// consultation de la "newTask.value"
+addTask.addEventListener("click", () => {
+  // qui génère une variable "valeur" qui correspont à la "value" de "newTask"
+  let valeur = newTask.value;
+  console.log(valeur); // consultation de la "newTask.value"
 });
