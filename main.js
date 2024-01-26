8e7fe195400b755bd35f86b7a2e08b1ebbde1ee2
gus0660 <groch1@hotmail.fr>
2024-01-26 17:21:23 +0100

    commentaire

--- main.js
+++ main.js
@@ -8,15 +8,18 @@ function créerBalise(element) {
 const newTask = créerBalise("#newTask");
 const addTask = créerBalise("#addTask");
 const tasksList = créerBalise("#tasksList");
+
 // pour commencer il faut : créé une détection du click sur "ajouter" donc "addEventListener" sur "addTask" nous donne un évenement "click" et la fonction qui en découle.
 addTask.addEventListener("click", () => {
   let valeur = newTask.value; // qui génère une variable "valeur" qui correspont à la "value" de "newTask"
+
 // et une condition "if" car il nous faut une interrogation : si on rentre des characteres alors le reste des fonctions se mettent en marche.
   if (isNaN(newTask.value) == true) {// si la valeur "value" de "newTask" est "isNaN" (c'est not a number) et qui renvoie "true" alors :
     let newLi = document.createElement("li");//je génère une variable "newLi" en créant un element "li" (balise html)
     newLi.textContent = valeur;// je la remplis (textContent) avec la let "valeur" (texte saisi)
     newLi.id = "boite";// au passage je lui donne un id pour pouvoir l'habiller avec du css
     tasksList.appendChild(newLi);//je fait de "newLi" l'enfant de "tasksList" pour rattacher la "li" au "ul"
+
 // ok je peux créer un bouton "suprimer" pour faire disparaitre chaque "newLi" ainsi généré
     let supr = document.createElement("button");// je cré une variable "supr" qui cré un element "button" (balise html)
     supr.textContent = "suprimer";// je le remplis "textContent" avec les characteres : "suprimer"
@@ -33,5 +36,5 @@ addTask.addEventListener("click", () => {
 // je commence le "localStorage" par la fonction "saveLi" que je dois appeler à la fin de l'addEventListener de addTask (addTask.addEventListener)
       function saveLi() {
         const textlList = []; // créations d'une constante "tablList" qui est un tableau : [] vide
-        const meslists = document.querySelectorAll("li");
+        const meslists = document.querySelectorAll("li");// creation d'une constante qui, sur le DOM(document) va prendre tout(all) les "li"
       }
